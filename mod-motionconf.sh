b9c4adea6c9146258e07b4cc36e027e3ce13d1dd
Techscapades <76870663+techscapades@users.noreply.github.com>
2025-02-04 19:38:22 +0800

    Update mod-motionconf.sh

--- mod-motionconf.sh
+++ mod-motionconf.sh
@@ -18,7 +18,7 @@ echo "The value from the cam_name file is: $value"
 
 echo "Setting camera parameters in /motion-streaming/${value} to /motion-streaming/motion.conf"
 sed -i "s|^video_device .*|video_device $(cat /motion-streaming/${value}/cam_path.txt)|" /motion-streaming/motion.conf
-sed -i "s|^webcontrol_port .*|webcontrol_port $(cat /motion-streaming/${value}/webcontrolport.txt)|" /motion-streamin>
+sed -i "s|^webcontrol_port .*|webcontrol_port $(cat /motion-streaming/${value}/webcontrolport.txt)|" /motion-streaming/motion.conf
 sed -i "s|^stream_port .*|stream_port $(cat /motion-streaming/${value}/streamport.txt)|" /motion-streaming/motion.conf
 echo "Copying /motion-streaming/motion.conf to /etc/motion"
 cp -f /motion-streaming/motion.conf /etc/motion/
