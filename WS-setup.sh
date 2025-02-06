295f7bee30330174ed8082dcf01c3b9d5b70566a
Techscapades <76870663+techscapades@users.noreply.github.com>
2025-02-06 13:07:32 +0800

    Update WS-setup.sh

--- WS-setup.sh
+++ WS-setup.sh
@@ -5,7 +5,7 @@ echo "Updating and upgrading system..."
 apt-get update && apt-get upgrade -y && apt autoremove -y
 
 echo "Installing essential packages..."
-DEBIAN_FRONTEND=noninteractive apt install python3 python3-pip motion -y
+DEBIAN_FRONTEND=noninteractive apt install python3 python3-pip v4l-utils motion -y
 apt install nano curl wget git htop net-tools usbutils libusb-1.0-0 avahi-daemon -y
 
 echo "Cloning motion-streaming repository..."
